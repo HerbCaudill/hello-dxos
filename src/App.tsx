f6c0fcc3ecea39c0b733476eb119ba75466312df
Herb Caudill <herb@devresults.com>
2024-11-19 15:17:22 +0100

    remove todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-//
-// Copyright 2023 DXOS.org
-//
-
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import {
   Navigate,
   RouterProvider,
@@ -10,83 +6,79 @@ import {
   useNavigate,
   useParams,
   useSearchParams,
-} from 'react-router-dom';
+} from 'react-router-dom'
 
-import { create } from '@dxos/echo-schema';
-import { ClientProvider, useShell } from '@dxos/react-client';
-import { useSpace, useQuery, Filter } from '@dxos/react-client/echo';
+import { create } from '@dxos/echo-schema'
+import { ClientProvider, useShell } from '@dxos/react-client'
+import { useSpace, useQuery, Filter } from '@dxos/react-client/echo'
 
-import { TaskList } from './TaskList';
-import { getConfig } from './config';
-import { TaskType } from './types';
+import { TaskList } from './TaskList'
+import { getConfig } from './config'
+import { TaskType } from './types'
 
 export const TaskListContainer = () => {
-  const { spaceKey } = useParams<{ spaceKey: string }>();
+  const { spaceKey } = useParams<{ spaceKey: string }>()
 
-  const space = useSpace(spaceKey);
-  const tasks = useQuery<TaskType>(space, Filter.schema(TaskType));
-  const shell = useShell();
+  const space = useSpace(spaceKey)
+  const tasks = useQuery<TaskType>(space, Filter.schema(TaskType))
+  const shell = useShell()
 
   return (
     <TaskList
       tasks={tasks}
       onInviteClick={async () => {
         if (!space) {
-          return;
+          return
         }
-        void shell.shareSpace({ spaceKey: space?.key });
-        // TODO: desired API to teach shell how to form share URLs
-        // void shell.shareSpace({ spaceKey: space?.key, invitationUrl: (invitationCode) => `/space/${space.key}?spaceInvitationCode=${invitationCode}` });
+        void shell.shareSpace({ spaceKey: space?.key })
       }}
-      onTaskCreate={(newTaskTitle) => {
-        const task = create(TaskType, { title: newTaskTitle, completed: false });
-        space?.db.add(task);
+      onTaskCreate={newTaskTitle => {
+        const task = create(TaskType, { title: newTaskTitle, completed: false })
+        space?.db.add(task)
       }}
-      onTaskRemove={(task) => {
-        space?.db.remove(task);
+      onTaskRemove={task => {
+        space?.db.remove(task)
       }}
       onTaskTitleChange={(task, newTitle) => {
-        task.title = newTitle;
+        task.title = newTitle
       }}
       onTaskCheck={(task, checked) => {
-        task.completed = checked;
+        task.completed = checked
       }}
     />
-  );
-};
+  )
+}
 
 export const Home = () => {
-  const space = useSpace();
-  const shell = useShell();
-  const [search, setSearchParams] = useSearchParams();
-  const invitationCode = search.get('spaceInvitationCode');
-  const deviceInvitationCode = search.get('deviceInvitationCode');
-  const navigate = useNavigate();
+  const space = useSpace()
+  const shell = useShell()
+  const [search, setSearchParams] = useSearchParams()
+  const invitationCode = search.get('spaceInvitationCode')
+  const deviceInvitationCode = search.get('deviceInvitationCode')
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (deviceInvitationCode) {
-      // TODO(???): desired API for joining a device.
-      // shell.joinDevice({ invitationCode: deviceInvitationCode });
-      setSearchParams((p) => {
-        p.delete('deviceInvitationCode');
-        return p;
-      });
+      setSearchParams(p => {
+        p.delete('deviceInvitationCode')
+        return p
+      })
     } else if (invitationCode) {
-      setSearchParams((p) => {
-        p.delete('spaceInvitationCode');
-        return p;
-      });
+      setSearchParams(p => {
+        p.delete('spaceInvitationCode')
+        return p
+      })
       void (async () => {
-        const { space } = await shell.joinSpace({ invitationCode });
+        const { space } = await shell.joinSpace({ invitationCode })
         if (space) {
-          navigate(`/space/${space.key}`);
+          navigate(`/space/${space.key}`)
         }
-      })();
+      })()
     }
-  }, [invitationCode, deviceInvitationCode]);
+  }, [invitationCode, deviceInvitationCode])
 
-  return space ? <Navigate to={`/space/${space.key}`} /> : null;
-};
+  return space ? <Navigate to={`/space/${space.id}`} /> : null
+}
 
 const router = createBrowserRouter([
   {
@@ -97,29 +89,29 @@ const router = createBrowserRouter([
     path: '/',
     element: <Home />,
   },
-]);
+])
 
 const createWorker = () =>
   new SharedWorker(new URL('./shared-worker', import.meta.url), {
     type: 'module',
     name: 'dxos-client-worker',
-  });
+  })
 
 export const App = () => {
   return (
     <ClientProvider
       config={getConfig}
       createWorker={createWorker}
-      shell='./shell.html'
+      shell="./shell.html"
       types={[TaskType]}
-      onInitialized={async (client) => {
-        const searchParams = new URLSearchParams(location.search);
+      onInitialized={async client => {
+        const searchParams = new URLSearchParams(location.search)
         if (!client.halo.identity.get() && !searchParams.has('deviceInvitationCode')) {
-          await client.halo.createIdentity();
+          await client.halo.createIdentity()
         }
       }}
     >
       <RouterProvider router={router} />
     </ClientProvider>
-  );
-};
+  )
+}
